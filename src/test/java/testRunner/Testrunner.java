c9fb1d8033d183c11409303a8cd087f4edf2468d
sulakshana-763 <sulakshana763@gmail.com>
2025-02-05 21:48:01 -0800

    Added testrunner and removed one dependency in pom

--- src/test/java/testRunner/Testrunner.java
+++ src/test/java/testRunner/Testrunner.java
@@ -1,5 +1,11 @@
 package testRunner;
 
-public class Testrunner {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+		features = "src/test/resources/features",
+		glue = {"src/test/java/stepDefinition_requests"})
+public class Testrunner extends AbstractTestNGCucumberTests {
 
 }
